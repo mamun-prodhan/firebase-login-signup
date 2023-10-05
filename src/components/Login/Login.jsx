c82bd075b3518efd3aa318a2230e822fcde1009a
mamun-prodhan <mamunpr9@gmail.com>
2023-10-05 14:00:18 +0600

    feat: forgot password added

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,12 +1,16 @@
-import { signInWithEmailAndPassword } from "firebase/auth";
+import {
+  sendPasswordResetEmail,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
 import auth from "../../firebase/firebase.config";
-import { useState } from "react";
+import { useRef, useState } from "react";
 import { Link } from "react-router-dom";
 
 const Login = () => {
   const [loginError, setLoginError] = useState("");
   const [loginErrorCode, setLoginErrorCode] = useState("");
   const [loggedInUser, setLoggedInUser] = useState("");
+  const emailRef = useRef(null);
   const handleSubmit = (e) => {
     e.preventDefault();
     const email = e.target.email.value;
@@ -27,6 +31,27 @@ const Login = () => {
         console.log(errorCode, errorMessage);
       });
   };
+
+  const handleForgetPassword = () => {
+    const email = emailRef.current.value;
+    if (!email) {
+      console.log("Provide an email");
+    } else if (
+      !/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/.test(email)
+    ) {
+      console.log("Please write a valid email");
+      return;
+    }
+    // send validation email
+    sendPasswordResetEmail(auth, email)
+      .then(() => {
+        alert("Please check your email");
+      })
+      .catch((error) => {
+        const errorMessage = error.message;
+        console.log(errorMessage);
+      });
+  };
   return (
     <div>
       <div className="w-1/2 mx-auto">
@@ -34,6 +59,7 @@ const Login = () => {
         <form onSubmit={handleSubmit}>
           <input
             className="py-3 px-3 mb-4 w-3/4"
+            ref={emailRef}
             type="email"
             name="email"
             id=""
@@ -48,6 +74,15 @@ const Login = () => {
             placeholder="Password"
           />
           <br />
+          <label className="label" htmlFor="">
+            <a
+              onClick={handleForgetPassword}
+              href="#"
+              className="lable-text-alt link link-hover"
+            >
+              Forgot Password?
+            </a>
+          </label>
           {loginError && <p className="font-bold text-red-600">{loginError}</p>}
           {loggedInUser && (
             <p className="font-bold text-green-600">Successfully Logged In</p>
